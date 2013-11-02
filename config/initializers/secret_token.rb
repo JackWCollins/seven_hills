36d24ebc441840729c4697fb1006f626b935caa0
Jack Collins <BlueSkyFlyer2@gmail.com>
2013-11-02 16:30:14 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SevenHills::Application.config.secret_key_base = '71c84d418e6b94499773e6ba8abbe30f29787301795dc65de6338299f71d9012d8ce4d9ff40ddda7c324c50b932136f824ff8d03a86513aa9cadcae412b1d14f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SevenHills::Application.config.session_store :cookie_store, key: '_seven_hills_session'
