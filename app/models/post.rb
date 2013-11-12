1ec812245a070d44cd410a23d2962c1135c721c2
Jack Collins <BlueSkyFlyer2@gmail.com>
2013-11-11 20:33:36 -0600

    Added validations to user and post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,7 @@ class Post < ActiveRecord::Base
 
   belongs_to :creator, class_name: 'User', foreign_key: :user_id
 
+  validates :title, presence: true, length: {minimum: 3}
+  validates :body, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
 
   has_many :posts
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
 end
