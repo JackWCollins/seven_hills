c19025262dc7e2c44fe699f76f90d76f28ccd86d
Jack Collins <BlueSkyFlyer2@gmail.com>
2013-11-19 17:39:29 -0600

    Filling out show user page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_action :set_user, only: [:show, :edit, :update]
+
 
 	def index
 		if logged_in?
@@ -34,6 +36,10 @@ class UsersController < ApplicationController
 	end
 
   private
+
+  def set_user
+  	@user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password)
