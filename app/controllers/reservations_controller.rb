40d307407de5b0a63bf26777558f7423b37c5483
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-03-18 21:23:49 -0500

    No longer require user to be logged in to create reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,5 @@
 class ReservationsController < ApplicationController
-	before_action :require_user, except: [:new, :home, :find, :search]
+	before_action :require_user, except: [:new, :home, :find, :search, :show, :create]
 
   def home
   end
@@ -9,25 +9,25 @@ class ReservationsController < ApplicationController
   end
 
   def new
-		if !logged_in?
-			flash[:success] = "We need your contact information before creating a reservation. Please register below!"
-			redirect_to register_path
-		end
+		#if !logged_in?
+			#flash[:success] = "We need your contact information before creating a reservation. Please register below!"
+			#redirect_to register_path
+		#end
 		@reservation = Reservation.new
 	end
 
 	def create
-		@student = Student.new
-		@student.update_attributes(first_name: current_user.first_name, last_name: current_user.last_name, email: current_user.email, phone: current_user.phone, age: current_user.age, street_address: current_user.street_address, city: current_user.city, state: current_user.state)
+		#@student = Student.new
+		#@student.update_attributes(first_name: current_user.first_name, last_name: current_user.last_name, email: current_user.email, phone: current_user.phone, age: current_user.age, street_address: current_user.street_address, city: current_user.city, state: current_user.state)
 		@reservation = Reservation.new(reservation_params)
-		@reservation.creator = current_user
-		@reservation.students << @student
+		#@reservation.creator = current_user
+		#@reservation.students << @student
 		Group.transaction do
 			begin
-				@student.save!
+				#@student.save!
 				@reservation.save!
 				flash[:notice] = "Group created! You have been added as the first skydiver in the group."
-				redirect_to reserve_openings_path(student_id: @student.id)
+				redirect_to reservation_path(@reservation)#reserve_openings_path(student_id: @student.id)
 			rescue Exception => e
 				flash[:danger] = "The group could not be created. Please try again. If the problem persists, please call the office."
 				render :new
