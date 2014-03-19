795a2e9d4d20065822fae96ebe60283474102e3d
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-03-18 20:45:18 -0500

    Calendar fix

--- app/assets/javascripts/datepicker.js
+++ app/assets/javascripts/datepicker.js
@@ -1,8 +1,7 @@
-$(document).ready(function() {
-  $('#opening_date').datepicker({
-  	autoclose: true
-  });
-  $('#reservation-date').datepicker({
-  	autoclose: true
-  });
-});
+var ready = function() {
+  $('#opening_date').datepicker({autoclose: true});
+  $('#reservation-date').datepicker({autoclose: true});
+}
+
+$(document).ready(ready);
+$(document).on('page:load', ready);
