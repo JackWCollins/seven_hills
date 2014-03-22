3dd0644c7421a8d7162df6f705199e38570122a1
Jack Collins <BlueSkyFlyer2@gmail.com>
2014-03-22 17:29:43 -0500

    Further UI improvements all over the site

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,4 +7,5 @@ class Student < ActiveRecord::Base
 	validates_presence_of :first_name, :last_name, :email, :phone, :age, :street_address, :city, :state
 
 	validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 18}
+	validates :state, length: { is: 2 }
 end
